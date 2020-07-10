ff7e43e8dfc9395dd4315b95dcc6f8d629508e97
Ruhel <ruhel241@gmail.com>
2020-07-10 16:28:53 +0600

    Added Laravel & Vue Image

--- resources/js/HomeApp.vue
+++ resources/js/HomeApp.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <!-- <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -11,7 +11,21 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
+
+      <div class="flex-center position-ref full-height">
+              <div class="content">
+                <div class="title m-b-md">
+                    <img src="https://res.cloudinary.com/dtfbvvkyp/image/upload/v1566331377/laravel-logolockup-cmyk-red.svg" width="400">
+                    <img src="https://cli.vuejs.org/favicon.png" width="120">
+                </div>
+
+                <div class="links">
+                    <a href="https://github.com/ruhel241/laravel-vue-boilerplate">GitHub</a>
+                </div>
+            </div>
+        </div>
+
 </template>
 
 <script>
@@ -21,3 +35,49 @@
         }
     }
 </script>
+
+<style scoped>
+   
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 84px;
+    }
+
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+        
+</style>
